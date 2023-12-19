0bbe418b860ac2b6e0d2152a3d456cda7e019719
Pharoah_devil <97384627+RishiKumar156@users.noreply.github.com>
2023-12-19 22:20:13 +0530

    only for progress

---
+++ reverse.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self,value):
+        self.value = value 
+        self.next = None 
+        
+
+class LinkedsList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+    
+    def insert(self, value):
+        temp  = self.head 
+        while temp.next:
+            temp = temp.next 
+        temp.next = Node(value)
+    
+
+def reverse_ll(head):
+    prev = None
+    while True:
+        next_node = head.next 
+        prev = head.next
+        head = next_node
